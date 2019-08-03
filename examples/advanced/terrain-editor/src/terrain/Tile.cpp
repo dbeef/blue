0eae57529b11f382c09cbd973c4f57b9bd0531c5
0xdbeef <daniel.zalega@gmail.com>
2019-08-03 18:27:28 +0200

    Tuned up map colors

--- examples/advanced/terrain-editor/src/terrain/Tile.cpp
+++ examples/advanced/terrain-editor/src/terrain/Tile.cpp
@@ -4,15 +4,32 @@ Vertices Tile::get_vertices_translated(float x_pos, float y_pos, const glm::vec3
 {
 	glm::vec3 normal = { 0.0, 1.0f, 0.0f };
 
-	Vertices vertices =
-	{
-		/* Vertex pos */  0.0f + x_pos, 0.0f,  0.0f + y_pos, /* Normal */normal.x, normal.y, normal.z,/* Color */ color_1.x, color_1.y, color_1.z,
-		/* Vertex pos */  0.0f + x_pos, 0.0f,  1.0f + y_pos, /* Normal */normal.x, normal.y, normal.z,/* Color */ color_2.x, color_2.y, color_2.z,
-		/* Vertex pos */  1.0f + x_pos, 0.0f,  1.0f + y_pos, /* Normal */normal.x, normal.y, normal.z,/* Color */ color_2.x, color_2.y, color_2.z,
-		/* Vertex pos */  1.0f + x_pos, 0.0f,  0.0f + y_pos, /* Normal */normal.x, normal.y, normal.z,/* Color */ color_2.x, color_2.y, color_2.z,
-	};
 
-	return vertices;
+	int random_variable = std::rand();
+	if (random_variable % 2)
+	{
+		Vertices vertices =
+		{
+			/* Vertex pos */  0.0f + x_pos, 0.0f,  0.0f + y_pos, /* Normal */normal.x, normal.y, normal.z,/* Color */ color_1.x, color_1.y, color_1.z,
+			/* Vertex pos */  0.0f + x_pos, 0.0f,  1.0f + y_pos, /* Normal */normal.x, normal.y, normal.z,/* Color */ color_2.x, color_2.y, color_2.z,
+			/* Vertex pos */  1.0f + x_pos, 0.0f,  1.0f + y_pos, /* Normal */normal.x, normal.y, normal.z,/* Color */ color_2.x, color_2.y, color_2.z,
+			/* Vertex pos */  1.0f + x_pos, 0.0f,  0.0f + y_pos, /* Normal */normal.x, normal.y, normal.z,/* Color */ color_2.x, color_2.y, color_2.z,
+		};
+		return vertices;
+	}
+	else
+	{
+		// Same as first, but rotated 90 degress clockwise:
+
+		Vertices vertices =
+		{
+			/* Vertex pos */  0.0f + x_pos, 0.0f,  1.0f + y_pos, /* Normal */normal.x, normal.y, normal.z,/* Color */ color_1.x, color_1.y, color_1.z,
+			/* Vertex pos */  1.0f + x_pos, 0.0f,  1.0f + y_pos, /* Normal */normal.x, normal.y, normal.z,/* Color */ color_2.x, color_2.y, color_2.z,
+			/* Vertex pos */  1.0f + x_pos, 0.0f,  0.0f + y_pos, /* Normal */normal.x, normal.y, normal.z,/* Color */ color_2.x, color_2.y, color_2.z,
+			/* Vertex pos */  0.0f + x_pos, 0.0f,  0.0f + y_pos, /* Normal */normal.x, normal.y, normal.z,/* Color */ color_2.x, color_2.y, color_2.z,
+		};
+		return vertices;
+	}
 }
 
 Attributes Tile::get_attributes()
