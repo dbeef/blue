bedf1c852241d5048271307424615fbc73731766
0xdbeef <daniel.zalega@gmail.com>
2019-08-11 11:14:58 +0200

    Added auto copying DLL's

--- cmake/dependencies/Windows.cmake
+++ cmake/dependencies/Windows.cmake
@@ -8,8 +8,10 @@ function(blue_find_dependencies_windows)
     find_package(SDL2 REQUIRED)
     find_package(spdlog REQUIRED)
 
-    find_library(ASSIMP_LIB NAMES "assimp-vc140-mt.lib")
-    message(STATUS "[BLUE] Assimp library path: ${ASSIMP_LIB}")
+    find_library(BLUE_ASSIMP_LIB NAMES "assimp-vc140-mt.lib")
+    get_target_property(ASSIMP_DLL assimp::assimp IMPORTED_LOCATION_RELEASE)
+    set(BLUE_ASSIMP_DLL ${ASSIMP_DLL} PARENT_SCOPE)
+    set(BLUE_ASSIMP_DLL ${ASSIMP_DLL})
 
     target_link_libraries(Dependencies INTERFACE
             SDL2::SDL2-static
@@ -17,8 +19,7 @@ function(blue_find_dependencies_windows)
             glad
             spdlog::spdlog
             ${OPENGL_LIBRARIES}
-            # TODO: Use BLUE_ASSIMP_PATH instead.
-            ${ASSIMP_LIB}
+            ${BLUE_ASSIMP_LIB}
             stb
             imgui
             )
@@ -27,16 +28,14 @@ function(blue_find_dependencies_windows)
     add_definitions(-DNOMINMAX)
     add_definitions(-DWIN32_LEAN_AND_MEAN)
 
-    if (${CMAKE_BUILD_TYPE} STREQUAL "Debug")
-        get_property(BLUE_ASSIMP_PATH TARGET assimp::assimp PROPERTY IMPORTED_LOCATION_DEBUG)
-    else ()
-        get_property(BLUE_ASSIMP_PATH TARGET assimp::assimp PROPERTY IMPORTED_LOCATION_RELEASE)
-    endif ()
+    message(STATUS "[BLUE] Assimp lib path: ${BLUE_ASSIMP_LIB}")
+    message(STATUS "[BLUE] Assimp dll path: ${BLUE_ASSIMP_DLL}")
 
-    set(BLUE_ASSIMP_PATH ${ASSIMP_PATH} PARENT_SCOPE)
-    set(BLUE_ASSIMP_PATH ${ASSIMP_PATH})
+endfunction()
 
-    message(STATUS "[BLUE] Path to assimp: ${BLUE_ASSIMP_PATH}")
-
-
+function(blue_windows_deploy_dynamic_dependencies target_name)
+    add_custom_command(TARGET ${target_name} PRE_BUILD
+            COMMAND ${CMAKE_COMMAND} -E copy
+            ${BLUE_ASSIMP_DLL} "$<TARGET_FILE_DIR:${target_name}>/../"
+            )
 endfunction()
